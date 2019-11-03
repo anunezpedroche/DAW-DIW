03ac98d1c6ea49464436b7554eef15ba4f7ee2a5
Adrián Núñez <anunezpedroche@gmail.com>
2019-11-03 08:33:24 +0100

    Fin ejercicio yasuo
    Subida ejercicio yasuo

--- Ejercicios/Tarea09/drumkit.js
+++ Ejercicios/Tarea09/drumkit.js
@@ -1,18 +1,23 @@
 
 function capturarAudio(e){
-    console.log(e.keyCode);
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-
+    let div = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if(!audio) return;
     audio.currentTime = 0;
+    console.log(div);
+    
+    div.classList.add("transitioning");
+    div.classList.remove("key");
+    console.log(div);
     audio.play();
-
-    audio.classList.add("playing");
 }
 
-function removerSonido(e){
-    e.classList.remove("transitioning");
+function removerSonido(){
+    console.log("HOLA");
+    this.classList.remove("transitioning");
+    this.classList.add("key");
 }
 const keys = document.querySelectorAll(".key");
+
 keys.forEach(key =>key.addEventListener("transitionend",removerSonido));
 window.addEventListener("keypress",capturarAudio);
