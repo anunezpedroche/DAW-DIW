222f2d7898dea79409cfc78eb2b31870b3ad8cd8
Adrián Núñez <anunezpedroche@gmail.com>
2019-11-24 13:08:30 +0100

    Comments added
    Added comments to code

--- Tema 3/Tarea01/js/migracion.js
+++ Tema 3/Tarea01/js/migracion.js
@@ -7,46 +7,67 @@ email : <berlanas_ang@gva.es>
 
 */
 
-/*
-
-FUNCIONES PERDIDAS
-^(;,;)^
-
-*/
 let labelStep = 1;
 let prog = 2;
 let finalMsg = 3;
 let clicked=  true;
+
 function promesaMigrar(x){
+
     return new Promise(resolve => {
+
         setTimeout(() => {
+
             resolve(x);
+
         }, 50);
+
     });
+
 }
+
 function clicky(){
+
     if(clicked==true){
+
         startMigration();
+
     }
+
 }
 
 async function startMigration(){
+
     clicked = false;
+    // We catch the DOM elements that we going to work
     let label = document.querySelector(`steplabel[data-step="${labelStep}"]`);
     let progress = document.querySelector(`progress[data-step="${prog}"]`);
     let msg = document.querySelector(`finalmsg[data-step="${finalMsg}"]`);
+
+    //Add each class to DOM elements
     label.classList.add("estabaEscondido");
     progress.classList.add("estabaEscondido");
+
+    //Increment value in progress-bar
     for(var i = 0;i<100;i++){
+
         progress.value = i;
-        let promesa = promesaMigrar(10);
-        await promesa;
+        //Adding a promise awaiting a short time
+        let promesa =  await promesaMigrar(10);
+
     }
+
+    //Once progress-bar it's full, then show "Completado"
     msg.classList.add("estabaEscondido");
+
+    //Save the finalMsg because we going to increment their value and we need current value
     let transit = document.querySelector(`finalmsg[data-step="${finalMsg}"]`);
+
     labelStep +=3;
     prog +=3;
     finalMsg += 3;
+
+    //We call again to startMigration
     transit.addEventListener("transitionend",startMigration);
 }
 
