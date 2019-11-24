5785c0a23e722fbce843a7d3429273ae72fe05ac
Adrián Núñez <anunezpedroche@gmail.com>
2019-11-24 12:48:02 +0100

    Updated JS
    Last version of JS file. Functionally working. Only design is need to end.

--- Tema 3/Tarea01/js/migracion.js
+++ Tema 3/Tarea01/js/migracion.js
@@ -14,21 +14,48 @@ FUNCIONES PERDIDAS
 
 */
 let labelStep = 1;
+let prog = 2;
+let finalMsg = 3;
+let clicked=  true;
+function promesaMigrar(x){
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(x);
+        }, 50);
+    });
+}
+function clicky(){
+    if(clicked==true){
+        startMigration();
+    }
+}
 
-
-function startMigration(){
+async function startMigration(){
+    clicked = false;
     let label = document.querySelector(`steplabel[data-step="${labelStep}"]`);
-    label.style.opacity = "0.9";
-    console.log(label);
-    // Fragmentos perdidos
-    // ^(;,;)^
+    let progress = document.querySelector(`progress[data-step="${prog}"]`);
+    let msg = document.querySelector(`finalmsg[data-step="${finalMsg}"]`);
+    label.classList.add("estabaEscondido");
+    progress.classList.add("estabaEscondido");
+    for(var i = 0;i<100;i++){
+        progress.value = i;
+        let promesa = promesaMigrar(10);
+        await promesa;
+    }
+    msg.classList.add("estabaEscondido");
+    let transit = document.querySelector(`finalmsg[data-step="${finalMsg}"]`);
+    labelStep +=3;
+    prog +=3;
+    finalMsg += 3;
+    transit.addEventListener("transitionend",startMigration);
 }
 
 function init(){
     console.info(" * Init envirnoment ");
 
     // Set click function on button
-    document.querySelector("button").addEventListener("click",startMigration);
+        document.querySelector("button").addEventListener("click",clicky);
+    
 }
 
 // Init the environment when all is ready
