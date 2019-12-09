315d2b13982fb6e0ff56bc4430a9d7607bbe8893
Adrián Núñez <anunezpedroche@gmail.com>
2019-12-09 20:37:30 +0100

    Updating JS
    Updating JS with full functionally

--- NodeJS/DatosAbiertos/public/main.js
+++ NodeJS/DatosAbiertos/public/main.js
@@ -2,15 +2,36 @@ let disparador = false;
 
 function buscador(){
     disparador = document.getElementById('buscador').addEventListener('input',trigger);
-    fetch('http://mapas.valencia.es/lanzadera/opendata/res_aceite/JSON').then(function(response){ return response.json();}).then(function(myJson){filtrazo(myJson.features);})
+    fetch('http://mapas.valencia.es/lanzadera/opendata/res_aceite/JSON').then(response => {
     
+        return response.json();
     
+    }).then(respuesta =>{
     
-    
+    console.log(respuesta.features[43]);
+    const result = respuesta.features.filter(filtrar);
+    let listar = document.createElement('ul');
+
+        result.forEach(aceite=>{
+
+            let listadito = document.createElement('li');
+            listadito.innerHTML=aceite.properties.direccion+" -- "+aceite.properties.centro+" --["+aceite.geometry.coordinates+"]";
+            listar.appendChild(listadito);
+            console.log(listadito);
+        });
+        document.querySelector(".resultados").innerHTML = "";
+        document.querySelector(".resultados").appendChild(listar);
+
+    });
 }
 
+function upper(){
+    document.getElementById('buscador').value = document.getElementById('buscador').value.toUpperCase();
+}  
+
 function init(){
-    buscador();
+    document.getElementById('buscar').addEventListener('click',buscador);
+    document.getElementById('buscador').addEventListener('input',upper);
 }
 
 function trigger(){
@@ -18,9 +39,9 @@ function trigger(){
     return true;
 }
 
-function filtrar(){
+function filtrar(elemento){
     let letra = document.getElementById('buscador').value;
-    return letra;
+    return elemento.properties.direccion.startsWith(letra);
 }
 
 function filtrazo(a){
