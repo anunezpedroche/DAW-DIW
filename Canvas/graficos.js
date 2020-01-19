b455dd69722e520ac31d4a7c4d84acd826c1bd28
Adrián Núñez <anunezpedroche@gmail.com>
2020-01-19 13:53:29 +0100

    Subiendo canvas

--- Canvas/graficos.js
+++ Canvas/graficos.js
@@ -7,12 +7,25 @@ function buildGrafico(){
 
     const canvas = document.querySelector('canvas');
     let ctx = canvas.getContext('2d');
-
-    ctx.fillStyle = "rgb(200,0,0)";
+    canvas.width = window.innerWidth;
+    /*ctx.fillStyle = "rgb(200,0,0)";
     ctx.fillRect (100, 100, 55, 50);
   
     ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-    ctx.fillRect (300, 300, 55, 50);
+    ctx.fillRect (300, 300, 55, 50);*/
+
+    ctx.fillStyle = "rgb(0,0,200)";
+    console.log(Math.PI*2);
+    let total = Math.PI*2;
+    let primerDato = (total / 1.3333333) * -1;
+    let segundoDato = (total/ 1.3333333);
+    let tercerDato = (total/1.3333333) * -1;
+    console.log(primerDato);
+    ctx.arc(100,100,50,0,primerDato);
+    ctx.arc(100,100,50,primerDato,segundoDato);
+    ctx.arc(100,100,50,segundoDato,tercerDato);
+
+    ctx.stroke();
 }
 
 function loadListeners(){
